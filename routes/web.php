5cab5c93df1395d9a6398e237e30cb1dea106bc7
Wendel Moro <wendelcmoro@gmail.com>
2025-01-25 15:45:39 -0300

    [Add] Adicionando Dashboard padrão com autenticação com laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,30 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth/login');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Rotas descritas a partir daqui são apenas acessíveis por ADMINs
+    Route::middleware('auth:admin')->group(
+        function() {
+            Route::get('/test', function () {
+                return 'teste123';
+            })->name('test');;
+        }
+    );
+});
+
+require __DIR__.'/auth.php';
